959a960b9b93a94c63646b539837dcdf226e3d74
Kuan-Fu <creazy_fu@hotmail.com>
2022-04-30 12:02:05 +0800

    modified post/patch

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,13 +1,24 @@
 const handleSuccess= require('../service.js/handleSuccess');
 const Post = require('../models/postsModel');
+const handleError = require('../service.js/handleError');
 const postsControllers = {
   getPostsData:async(req,res)=>{
     const data = await Post.find()
     handleSuccess(req,res,data)
   },
-  postPostsData:async(req,res,post)=>{
-    const data = await Post.create(post);
-    handleSuccess(req,res,data)
+  postPostsData:async(req,res,body)=>{
+    console.log('111')
+    try{
+      const post = JSON.parse(body);
+      if(post.name && post.tags && post.type && post.content){
+        const data = await Post.create(post);
+        handleSuccess(req,res,data)
+      }else{
+        handleError(req,res,'格式不齊全')
+      }
+    }catch{
+      handleError(req,res,'格式錯誤')
+    }
   },
   deleteSinglePostsData:async(req,res, id)=>{
     await Post.findByIdAndDelete({_id:id})
@@ -17,9 +28,15 @@ const postsControllers = {
     await Post.deleteMany();
     handleSuccess(req,res,'資料庫已清空')
   },
-  patchPostsData:async(req,res,id,post)=>{
-    const data = await Post.findByIdAndUpdate({_id:id},post,{new:true});
-    handleSuccess(req,res,data)
+  patchPostsData:async(req,res,id,body)=>{
+    try{
+      const post = JSON.parse(body);
+      const data = await Post.findByIdAndUpdate({_id:id},post,{new:true});
+      handleSuccess(req,res,data)
+    }catch(err){
+      handleError(req,res,'格式不齊全')
+    }
+    
   }
 }
 module.exports = postsControllers
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,14 @@
 const postsControllers = require('../controllers/postsController');
 const httpsControllers = require('../controllers/https');
-const handleError = require('../service.js/handleError');
 const routes = (req,res)=>{
   const {url ,method} = req;
   let body = '';
   req.on('data',chunk=> body += chunk);
-  if(url==='/posts' &&method==='GET'){
+  if(url==='/posts' && method==='GET'){
     postsControllers.getPostsData(req,res)
   }else if(url==='/posts' && method==='POST'){
     req.on('end',()=>{
-      try{
-        const post = JSON.parse(body);
-        if(post.name && post.tags && post.type && post.content){
-          postsControllers.postPostsData(req,res,post)
-        }else{
-          handleError(req,res,'格式不齊全')
-        }
-      }catch(err){
-        handleError(req,res,'格式錯誤')
-      }
+      postsControllers.postPostsData(req,res,body)
     });
   }else if(url==='/posts' &&method==='OPTIONS'){
     httpsControllers.cors(req,res);
@@ -29,13 +19,8 @@ const routes = (req,res)=>{
     postsControllers.deleteSinglePostsData(req,res,id)
   }else if(url.startsWith('/posts/') && method==='PATCH'){
     req.on('end',()=>{
-      try{
-        const post = JSON.parse(body);
-        const id = url.split('/').pop();
-        postsControllers.patchPostsData(req,res,id,post)
-      }catch{
-        handleError(req,res,'格式錯誤')
-      }
+      const id = url.split('/').pop();
+      postsControllers.patchPostsData(req,res,id,body);
     })
   }else{
     httpsControllers.errorParam(req,res)
